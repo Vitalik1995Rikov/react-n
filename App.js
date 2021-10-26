df291de469516448dcc3e62a7374a5f00bad0117
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-26 17:21:38 +0300

    RN4

--- App.js
+++ App.js
@@ -4,13 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { Costs } from './Funds/Costs';
+import Earnings from './Funds/Earnings';
+
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>123</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Costs" component={Costs} />
+        <Drawer.Screen name="Earnings" component={Earnings} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
